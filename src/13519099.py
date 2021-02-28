9eabab9c096449001848a77b858ed351efb46332
Thomas Ferdinand M <56783537+thomas-fm@users.noreply.github.com>
2021-02-28 16:08:44 +0700

    update komentar

--- src/13519099.py
+++ src/13519099.py
@@ -3,16 +3,22 @@
 # Tucil 2 : Decrease and Conquer
 
 # run : python Tucil2_13519099.py
+# berhasil di run menggunakan python versi 3.9.0
+
+# Representasi graf memanfaatkan kelebihan python menggunakan struktur array multidimensi
+# Bentuk representasi graf pada array multidimensi
+# Elemen G[i][0] adalah nama simpul, G[i][1] - G[i][n] adalah nama simpul masuk
+#  [
+#   [simpul1, simpul_masuk_1, simpul_masuk_2], 
+#   [simpul2, simpul_masuk_1],
+#   [simpul3]
+# ]
 
 # Berisi simpul-simpul hasil akhir topologicalSort
 # Bentuk fisik array : [[C1], [C2, C3], [C4,C5,C6]]
 # Menunjukkan semester 1 perlu mengambil matkul dengan kode C1, semester 2 mengambil C2 dan C3, semester 3 mengambil C4, C5, C6
 global result
 result = []
-
-# Berisi mata kuliah yang sudah selesai diambil saat itu
-global matkul_terambil
-matkul_terambil = []
 
 # Membuka dan membaca file
 def readFile(namaFile):
@@ -26,22 +32,17 @@ def readFile(namaFile):
     return matkul_dan_prereq
 
 # Fungsi menambahkan matkul yang saat itu pre requisitnya terpenuhi atau seolah-olah tidak memiliki pre requisit ke dalam list mata kuliah per semester
-# Fungsi kemudian mengubah kondisi graf dengan seolah-olah menghapus mata kuliah yang memiliki 0 derajat masuk
+# Fungsi kemudian mengubah kondisi graf dengan menghapus mata kuliah yang memiliki derajat masuk 0
 def hapusZeroInDegree(kumpulan_mata_kuliah):
     # List berisi mata kuliah yang harus diambil pada semester tertentu
     current_semester = []
-    # matkul_terambil = []
 
     # Untuk setiap data mata kuliah dengan pre requisitnya
-    # Bentuk representasi graf pada array multidimensi
-    # [[simpul1, simpul_derajat_masuk_1, simpul_derajat_masuk_2], 
-    # [simpul2, simpul_derajat_masuk_1]]
     # Graf tersimpan pada variabel mata kuliah
     for elm in kumpulan_mata_kuliah:
-        # Jika saat itu mata kuliah tidak memiliki pre requisit, maka tambahkan ke dalam array mata kuliah yang bisa diambil dalam satu semester itu (current_semester) dan array seluruh mata kuliah yang sudah diambil (matkul_terambil)
+        # Jika saat itu mata kuliah tidak memiliki pre requisit, maka tambahkan ke dalam array mata kuliah yang bisa diambil dalam satu semester itu (current_semester)
         if (len(elm) == 1):
             current_semester.append(elm[0])
-            matkul_terambil.append(elm[0])
     
     # Hapus mata kuliah yang diambil pada semester tersebut dari daftar mata kuliah yang masih perlu diambil (hapus simpul pada graf)
     for matkul in current_semester:
@@ -49,10 +50,11 @@ def hapusZeroInDegree(kumpulan_mata_kuliah):
 
     # Tambahkan data mata kuliah semester tersebut ke dalam array hasil
     result.append(current_semester)
+    return current_semester
 
-# Fungsi seolah-olah menghapus derajat masuk dari mata kuliah yang pra syaratnya sudah diambil atau berada pada array matkul_terambil
-def hapusSimpulBersisian(mata_kuliah):
-    for matkul in matkul_terambil:
+# Fungsi menghapus derajat masuk dari mata kuliah yang pra syaratnya sudah diambil atau berada pada array matkul_terambil
+def hapusSimpulBersisian(mata_kuliah, current_semester):
+    for matkul in current_semester:
         for sisa_matkul in mata_kuliah:
             # Jika ditemukan matkul pra syarat yang sudah terambil maka seolah-olah hapus sisi masuk graf
             if (sisa_matkul.count(matkul) == 1):
@@ -65,9 +67,9 @@ def topoSort(mata_kuliah):
         return
     else:
         # Pemanggilan fungsi penghapusan simpul derajat masuk 0
-        hapusZeroInDegree(mata_kuliah)
+        current_semester = hapusZeroInDegree(mata_kuliah)
         # Pemanggilan fungsi penghapusan sisi yang bersisian dengan simpul yang terhapus
-        hapusSimpulBersisian(mata_kuliah)
+        hapusSimpulBersisian(mata_kuliah, current_semester)
         # Pemanggilan rekursif
         topoSort(mata_kuliah)
 
@@ -79,6 +81,9 @@ def printHasil():
         for matkul in result[i]:
             print(matkul, end=" ")
         print("\r")
+
+    if len(result) > 8:
+        print("Sayang sekali sepertinya nanti kelulusan Anda harus ditunda jika mau mengikuti seluruh mata kuliah ini.")
     print("============++++============")
 
 # Fungsi menulis info matkul ke layar
